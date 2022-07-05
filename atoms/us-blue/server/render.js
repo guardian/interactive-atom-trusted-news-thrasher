9f26868932d197f8b5e83e45efe4197dd42b91d0
Harry Fischer <harry.fischer@guardian.co.uk>
2022-07-05 11:28:38 +0100

    add new july messaging

--- atoms/us-blue/server/render.js
+++
@@ -1,5 +0,0 @@
-import mainHTML from "./atoms/us-blue/server/templates/main.html!text"
-
-export async function render() {
-    return mainHTML;
-}
--- atoms/us-white/server/render.js
+++
@@ -1,5 +0,0 @@
-import mainHTML from "./atoms/us-white/server/templates/main.html!text"
-
-export async function render() {
-    return mainHTML;
-}
